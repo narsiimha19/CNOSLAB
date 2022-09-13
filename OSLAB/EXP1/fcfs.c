6dd7ab9fd42851cab3b73afab56d695ffda9ab3f
narsiimha19 <narasimhulu.cse@srit.ac.in>
2022-09-13 11:32:49 +0530

    My First Program FCFS.c is sucefully Executed

--- OSLAB/EXP1/fcfs.c
+++ OSLAB/EXP1/fcfs.c
@@ -6,8 +6,7 @@
 */
 // Source Code
 #include<stdio.h>
-#include<conio.h>
-main()
+int main()
 {
 int bt[20], wt[20], tat[20], i, n;
 float wtavg, tatavg;
@@ -32,5 +31,4 @@ for(i=0;i<n;i++)
 printf("\n\t P%d \t\t %d \t\t %d \t\t %d", i, bt[i], wt[i], tat[i]);
 printf("\nAverage Waiting Time -- %f", wtavg/n);
 printf("\nAverage Turnaround Time -- %f", tatavg/n);
-getch();
 }
